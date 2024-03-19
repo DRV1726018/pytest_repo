f8e060e5a67020628605627fb80226c46a16a5c3
DRV1726018 <davidramirezvillegas@gmail.com>
2024-03-19 16:21:16 -0500

    added error

--- test_calculadora.py
+++ test_calculadora.py
@@ -2,7 +2,7 @@ import pytest
 from calculadora import suma, resta
 
 def test_suma():
-    assert suma(2, 3) == 5
+    assert suma(2, 3) == "error"
     assert suma(-1, 1) == 0
     assert suma(0, 0) == 0
 
