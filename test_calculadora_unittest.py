b1ccc54b8c02d1a2c8f0b69363feb7d927ed9e39
DRV1726018 <davidramirezvillegas@gmail.com>
2024-03-19 21:05:36 -0500

    error añadido

--- test_calculadora_unittest.py
+++ test_calculadora_unittest.py
@@ -3,7 +3,7 @@ from calculadora import suma, resta
 
 class TestCalculadora(unittest.TestCase):
     def test_suma(self):
-        self.assertEqual(suma(2, 3), 5)
+        self.assertEqual(suma(2, 3), "error")
         self.assertEqual(suma(-1, 1), 0)
         self.assertEqual(suma(0, 0), 0)
 
